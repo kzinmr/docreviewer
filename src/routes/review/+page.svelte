78c323c7737d8aa6b65e1b1e9aec1b9e5a697ee6
Kazuki Inamura <kzinmr109@gmail.com>
2023-09-02 00:14:12 +0900

    update sidebar

--- src/routes/review/+page.svelte
+++ src/routes/review/+page.svelte
@@ -1,41 +1,67 @@
 <script lang='ts'>
   import TextArea from '$components/TextAreaAutoresize.svelte'
   import { paragraphs } from '$/store';
+
+  let sidebarWidth = 'w-full';
+  let sidebar: HTMLDivElement;
+  let resizer: HTMLDivElement;
+  let isSidebarVisible = true;
+  let startX: number;
+  let startWidth: number;
+
+  const handleMousedown = (e: MouseEvent) => {
+    startX = e.pageX;
+    startWidth = sidebar.offsetWidth;
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseup', () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+    });
+  };
+
+  const handleMouseMove = (e: MouseEvent) => {
+    const newWidth = startWidth + (e.pageX - startX);
+    sidebar.style.width = `${newWidth}px`;
+  };
+
+  function toggleSidebar() {
+    isSidebarVisible = !isSidebarVisible;
+    sidebar.classList.toggle('hidden');
+  }
 </script>
 
-<div class="drawer lg:drawer-open w-2/3">
+<div class="drawer relative">
   <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-  <div class="drawer-content flex flex-col items-center justify-center">
-    <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Review Items</label>
-
+  <div class="drawer-content items-start justify-start pt-4 h-screen overflow-y-auto">
+    {#if !isSidebarVisible}
+      <button on:click={toggleSidebar} class="btn btn-primary">Show Reviews</button>
+    {/if}
     <!-- Page content here -->
     {#if $paragraphs.length > 0}
       <div class="w-2/3 p-4 border rounded">
         <div class="flex flex-col h-screen overflow-y-auto p-8 space-y-4">
           {#each $paragraphs as paragraph}
-          <TextArea bind:value={paragraph} minRows={0} maxRows={40}/>  
-          <!-- <textarea class="textarea h-full max-h">{paragraph}</textarea> -->
+            <TextArea bind:value={paragraph} minRows={0} maxRows={40}/>  
           {/each}
         </div>
       </div>
-    {/if}  
-  </div> 
-  <div class="drawer-side">
-    <label for="my-drawer-2" class="drawer-overlay"></label> 
-    <ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-      <!-- Sidebar content here -->
-      <li><a>Review Item 1</a></li>
-      <li><a>Review Item 2</a></li>
-    </ul>
-  
+    {/if}
+  </div>
+  <div bind:this={sidebar} class="flex flex-row h-screen">
+    <div class="drawer-overlay {sidebarWidth} h-screen bg-base-200 text-base-content relative overflow-y-auto">
+      <button on:click={toggleSidebar} class="btn btn-block" tabindex="0">Hide Reviews</button>
+      <ul class="menu p-4 overflow-y-auto">
+        <!-- Sidebar content here -->
+        {#each Array(100) as _, n}
+          <li><a href="/">Review Item {n}</a></li>
+        {/each}
+      </ul>
+    </div>
+    {#if isSidebarVisible}
+      <div bind:this={resizer} on:mousedown|preventDefault={handleMousedown} class="cursor-ew-resize w-2 z-10 bg-base-300" tabindex="0" role="spinbutton"></div>
+    {/if}
   </div>
 </div>
 
 <style lang="postcss">
-  .textareas-container {
-    @apply w-2/3 p-4 border rounded;
-    margin: auto;
-    position: absolute;
-    top: 10%; bottom: 0; left: 0; right: 0;
-  }
+
 </style>
